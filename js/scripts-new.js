29a69c2ffa8c69c71eff540acb2f1f31b8ee85a3
VyacheslavVolkhin <vv@htmlworker.com>
2025-08-27 07:20:56 +0500

    add

---
+++ js/scripts-new.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+	
+	//files add
+	const fileBlocks = document.querySelectorAll('.js-field-file');
+	
+	fileBlocks.forEach(fileBlock => {
+		const fileInput = fileBlock.querySelector('.js-field-input');
+		const fileAttachButton = fileBlock.querySelector('.js-file-button-attach');
+		const fileDeleteButton = fileBlock.querySelector('.js-file-button-del');
+		const fileName = fileBlock.querySelector('.file-name');
+	
+		fileAttachButton.addEventListener('click', function() {
+			fileInput.click();
+		});
+	
+		fileInput.addEventListener('change', function() {
+			if (fileInput.files.length > 0) {
+				fileName.textContent = fileInput.files[0].name;
+				fileBlock.classList.add('file-active');
+			} else {
+				fileName.textContent = '';
+				fileBlock.classList.remove('file-active');
+			}
+		});
+	
+		fileDeleteButton.addEventListener('click', function(e) {
+			e.preventDefault();
+			fileName.textContent = '';
+			fileBlock.classList.remove('file-active');
+			fileInput.value = null;
+		});
+	});
+
+
+
+
+  
+});
