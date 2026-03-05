3395a12e7be41b34fc3b79b9559f541d5b5e4280
VyacheslavVolkhin <vv@htmlworker.com>
2026-03-05 17:08:06 +0500

    add

--- js/scripts-new.js
+++ js/scripts-new.js
@@ -3,82 +3,85 @@ document.addEventListener('DOMContentLoaded', function() {
 	//td button menu
 	const buttons = document.querySelectorAll('.js-td-button-menu');
 	const menu = document.querySelector('.tbl-menu-popup-box.dropdown-box');
-	
-	// Функция для позиционирования меню
-	function positionMenu(button) {
-		const buttonRect = button.getBoundingClientRect();
-		const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-		const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-		
-		menu.style.top = (buttonRect.bottom + scrollTop - 100) + 'px';
-		menu.style.left = (buttonRect.left + scrollLeft) + 'px';
-	}
-	
-	// Функция для открытия меню
-	function openMenu(button) {
-		positionMenu(button);
-		menu.classList.add('active-dotted');
-	}
-	
-	// Функция для закрытия меню
-	function closeMenu() {
-		menu.classList.remove('active-dotted');
-	}
-	
-	// Обработчик клика по кнопкам
-	buttons.forEach(button => {
-		button.addEventListener('click', function(e) {
-			e.stopPropagation(); // Предотвращаем всплытие события
+	if (buttons && menu) {
+		// Функция для позиционирования меню
+		function positionMenu(button) {
+			const buttonRect = button.getBoundingClientRect();
+			const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 			
-			if (menu.classList.contains('active-dotted')) {
+			menu.style.top = (buttonRect.bottom + scrollTop - 100) + 'px';
+			menu.style.left = (buttonRect.left + scrollLeft) + 'px';
+		}
+		
+		// Функция для открытия меню
+		function openMenu(button) {
+			positionMenu(button);
+			menu.classList.add('active-dotted');
+		}
+		
+		// Функция для закрытия меню
+		function closeMenu() {
+			menu.classList.remove('active-dotted');
+		}
+		
+		// Обработчик клика по кнопкам
+		buttons.forEach(button => {
+			button.addEventListener('click', function(e) {
+				e.stopPropagation(); // Предотвращаем всплытие события
+				
+				if (menu.classList.contains('active-dotted')) {
+					closeMenu();
+				} else {
+					openMenu(this);
+				}
+			});
+		});
+		
+		//Обработчик клика вне меню
+		document.addEventListener('click', function(e) {
+			const isClickInsideMenu = menu.contains(e.target);
+			const isClickOnButton = Array.from(buttons).some(button => button.contains(e.target));
+			
+			if (!isClickInsideMenu && !isClickOnButton) {
 				closeMenu();
-			} else {
-				openMenu(this);
 			}
 		});
-	});
-	
-	// Обработчик клика вне меню
-	document.addEventListener('click', function(e) {
-		const isClickInsideMenu = menu.contains(e.target);
-		const isClickOnButton = Array.from(buttons).some(button => button.contains(e.target));
 		
-		if (!isClickInsideMenu && !isClickOnButton) {
-			closeMenu();
-		}
-	});
-	
-	// Обновляем позицию при скролле
-	window.addEventListener('scroll', function() {
-		if (menu.classList.contains('active-dotted')) {
-			const activeButton = Array.from(buttons).find(button => 
-				button.classList.contains('active-button') // Добавьте класс для активной кнопки
-			);
-			
-			if (activeButton) {
-				positionMenu(activeButton);
+		// Обновляем позицию при скролле
+		window.addEventListener('scroll', function() {
+			if (menu.classList.contains('active-dotted')) {
+				const activeButton = Array.from(buttons).find(button => 
+					button.classList.contains('active-button') // Добавьте класс для активной кнопки
+				);
+				
+				if (activeButton) {
+					positionMenu(activeButton);
+				}
 			}
-		}
-	});
-	
-	// Обновляем позицию при ресайзе окна
-	window.addEventListener('resize', function() {
-		if (menu.classList.contains('active-dotted')) {
-			const activeButton = Array.from(buttons).find(button => 
-				button.classList.contains('active-button')
-			);
-			
-			if (activeButton) {
-				positionMenu(activeButton);
+		});
+		
+		// Обновляем позицию при ресайзе окна
+		window.addEventListener('resize', function() {
+			if (menu.classList.contains('active-dotted')) {
+				const activeButton = Array.from(buttons).find(button => 
+					button.classList.contains('active-button')
+				);
+				
+				if (activeButton) {
+					positionMenu(activeButton);
+				}
 			}
-		}
-	});
+		});
+	}
+	
 
 
 
 
 
 	
+
 
 
 	//select style
@@ -93,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			e.preventDefault();
 			const formWrap = this.closest('.tile-form-wrap');
 			if (formWrap) {
-				formWrap.classList.add('active-dotted');
+				formWrap.classList.toggle('active');
 				const input = formWrap.querySelector('.form-input');
 				if (input) input.focus();
 			}
@@ -266,4 +269,263 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		updateActiveSlide(swiperInstance);
 	});
+
+
+	//slider side
+	const slidersside = document.querySelectorAll(".slider-side");
+	
+	slidersside.forEach((container) => {
+		const swiperEl = container.querySelector(".swiper");
+		const paginationEl = container.querySelector(".slider-side-pagination");
+		const nextEl = container.querySelector(".button-slider-side-next");
+		const prevEl = container.querySelector(".button-slider-side-prev");
+	
+		if (!swiperEl) return;
+	
+		new Swiper(swiperEl, {
+			loop: false,
+			slidesPerGroup: 1,
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			autoHeight: false,
+			speed: 400,
+			pagination: {
+				el: paginationEl,
+				clickable: true,
+			},
+			autoplay: false,
+			navigation: {
+				nextEl: nextEl,
+				prevEl: prevEl,
+			},
+		});
+	});
+
+
+	//slider stories
+	const slidersstories = document.querySelectorAll(".slider-stories");
+	slidersstories.forEach((container) => {
+		const swiperEl = container.querySelector(".swiper");
+		const nextEl = container.querySelector(".button-slider-stories-next");
+		const prevEl = container.querySelector(".button-slider-stories-prev");
+	
+		if (!swiperEl) return;
+	
+		new Swiper(swiperEl, {
+			loop: true,
+			slidesPerGroup: 1,
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			autoHeight: false,
+			speed: 400,
+			pagination: false,
+			centeredSlides: true,
+			autoplay: false,
+			initialSlide: 7,
+			navigation: {
+				nextEl: nextEl,
+				prevEl: prevEl,
+			},
+		});
+	});
+
+
+	//slider stories inner
+	const slidersstoriesinner = document.querySelectorAll(".slider-stories-inner");
+	
+	slidersstoriesinner.forEach((container) => {
+		const swiperEl = container.querySelector(".swiper");
+		const paginationEl = container.querySelector(".slider-stories-inner-pagination");
+		const nextEl = container.querySelector(".button-slider-stories-inner-next");
+		const prevEl = container.querySelector(".button-slider-stories-inner-prev");
+	
+		if (!swiperEl) return;
+	
+		new Swiper(swiperEl, {
+			loop: false,
+			slidesPerGroup: 1,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: false,
+			speed: 400,
+			effect: 'fade',
+			fadeEffect: {
+				crossFade: true,
+			},
+			pagination: {
+				el: paginationEl,
+				clickable: true,
+			},
+			autoplay: false,
+			navigation: {
+				nextEl: nextEl,
+				prevEl: prevEl,
+			},
+		});
+	});
+
+
+	//slider tile
+	const sliderstile = document.querySelectorAll(".slider-tile");
+	
+	sliderstile.forEach((container) => {
+		const swiperEl = container.querySelector(".swiper");
+		const paginationEl = container.querySelector(".slider-tile-pagination");
+		const nextEl = container.querySelector(".button-slider-tile-next");
+		const prevEl = container.querySelector(".button-slider-tile-prev");
+	
+		if (!swiperEl) return;
+		const hasAutoHeight = container.dataset.height === "auto";
+	
+		new Swiper(swiperEl, {
+			loop: false,
+			slidesPerGroup: 1,
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			autoHeight: hasAutoHeight,
+			speed: 400,
+			pagination: {
+				el: paginationEl,
+				clickable: true,
+			},
+			autoplay: false,
+			navigation: {
+				nextEl: nextEl,
+				prevEl: prevEl,
+			},
+		});
+	});
+
+	//slider tile-inner
+	const sliderstileinner = document.querySelectorAll(".slider-tile-inner");
+	
+	sliderstileinner.forEach((container) => {
+		const swiperEl = container.querySelector(".swiper");
+		const paginationEl = container.querySelector(".slider-tile-inner-pagination");
+		const nextEl = container.querySelector(".button-slider-tile-inner-next");
+		const prevEl = container.querySelector(".button-slider-tile-inner-prev");
+	
+		if (!swiperEl) return;
+	
+		new Swiper(swiperEl, {
+			loop: false,
+			slidesPerGroup: 1,
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			autoHeight: false,
+			speed: 400,
+			pagination: {
+				el: paginationEl,
+				clickable: true,
+			},
+			autoplay: false,
+			navigation: {
+				nextEl: nextEl,
+				prevEl: prevEl,
+			},
+		});
+	});
+
+
+	//slider photos thumbs preview
+	document.querySelectorAll('.tiles-thumbs-slider-box').forEach(function(container) {
+		const thumbsEl = container.querySelector('.slider-photos-thumbs .swiper');
+		const mainEl = container.querySelector('.slider-photos-main .swiper');
+		const nextTBtn = container.querySelector('.button-slider-photos-thumbs-next');
+		const prevTBtn = container.querySelector('.button-slider-photos-thumbs-prev');
+	
+		const swiperPhotosPreview = new Swiper(thumbsEl, {
+			loop: false,
+			slidesPerGroup: 1,
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			threshold: 5,
+			watchSlidesVisibility: true,
+			watchSlidesProgress: true,
+			freeMode: false,
+			navigation: {
+				nextEl: nextTBtn,
+				prevEl: prevTBtn,
+			},
+		});
+		const swiperPhotosMain = new Swiper(mainEl, {
+			loop: false,
+			slidesPerGroup: 1,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: false,
+			speed: 400,
+			threshold: 5,
+			freeMode: false,
+			watchSlidesProgress: true,
+			navigation: false,
+			pagination: false,
+			thumbs: {
+				swiper: swiperPhotosPreview,
+			},
+		});
+	});
+
+
+	//slider srt
+	const sliderssrt = document.querySelectorAll(".slider-srt");
+	
+	sliderssrt.forEach((container) => {
+		const swiperEl = container.querySelector(".swiper");
+		const paginationEl = container.querySelector(".slider-srt-pagination");
+		const nextEl = container.querySelector(".button-slider-srt-next");
+		const prevEl = container.querySelector(".button-slider-srt-prev");
+	
+		if (!swiperEl) return;
+	
+		new Swiper(swiperEl, {
+			loop: false,
+			slidesPerGroup: 1,
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			autoHeight: false,
+			speed: 400,
+			pagination: {
+				el: paginationEl,
+				clickable: true,
+			},
+			autoplay:false,
+			navigation: {
+				nextEl: nextEl,
+				prevEl: prevEl,
+			},
+		});
+	});
+	
+	//slider gallery
+	const slidersgallery = document.querySelectorAll(".slider-gallery");
+	
+	slidersgallery.forEach((container) => {
+		const swiperEl = container.querySelector(".swiper");
+		const paginationEl = container.querySelector(".slider-gallery-pagination");
+		const nextEl = container.querySelector(".button-slider-gallery-next");
+		const prevEl = container.querySelector(".button-slider-gallery-prev");
+	
+		if (!swiperEl) return;
+	
+		new Swiper(swiperEl, {
+			loop: false,
+			slidesPerGroup: 1,
+			slidesPerView: 'auto',
+			spaceBetween: 0,
+			autoHeight: false,
+			freeMode: true,
+			speed: 400,
+			pagination: {
+				el: paginationEl,
+				clickable: true,
+			},
+			autoplay:false,
+			navigation: {
+				nextEl: nextEl,
+				prevEl: prevEl,
+			},
+		});
+	});
+	
 });
